84fc6cbc3493b37b7f991919317a210ece6a3122
Renzo Navarro <rcnavarrop@gmail.com>
2023-07-18 23:38:30 -0400

    Add docker to the stack

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  devtool: "source-map",
-  mode: "development",
-  target: "web",
-  entry: path.resolve(__dirname, "./src/index.jsx"),
+  devtool: 'source-map',
+  mode: 'development',
+  target: 'web',
+  entry: path.resolve(__dirname, './src/index.jsx'),
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
   },
   plugins: [
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Webpack bundled React Project",
-      template: path.resolve(__dirname, "./src/index.html"),
+      title: 'Webpack bundled React Project',
+      template: path.resolve(__dirname, './src/index.html'),
     }),
   ],
   devServer: {
-    port: "3000",
-    static: path.resolve(__dirname, "./dist"),
+    port: '3000',
+    static: path.resolve(__dirname, './dist'),
     open: true,
     hot: true,
     liveReload: true,
   },
   resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts"],
+    extensions: ['.js', '.jsx', '.json', '.ts'],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
         test: /\.(css)$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
